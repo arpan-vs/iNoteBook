6449ff6309b4e9bf5a74ead223d85ebbefa8872c
Arpan Shingala <202003013@daiict.ac.in>
2022-01-31 11:39:35 +0530

    Checkpoint1

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const connectToMongo = require('./db');
+const express = require('express');
+
+connectToMongo();
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+  
+app.listen(port, ()=>{
+    console.log(`iNoteBook listening at http://localhost:${port}`)
+})
