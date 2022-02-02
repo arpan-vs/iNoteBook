303952b4446f544b092e6803feb1fd9f3a30b314
Arpan Shingala <202003013@daiict.ac.in>
2022-02-02 18:41:10 +0530

    CheckPoint6

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'Arpanisagoodboy'
 // creat a User using: POST "/api/auth". no login required
 router.post(
     "/createuser",
@@ -29,13 +32,26 @@ router.post(
                         error: "Sorry a user with this email already exists",
                     });
             }
+
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password,salt);
+
             // create a user
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password,
+                password: secPass,
             });
-            res.json(user);
+            const data = {
+                user:{
+                    id: user.id
+                }
+            }
+            const authTotken = jwt.sign(data,JWT_SECRET);
+
+            // res.json(user);
+            res.json({authTotken})
+
         }// catch a error 
         catch (error) {
             console.error(error.message);
