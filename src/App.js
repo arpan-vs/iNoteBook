7d4fda380b1f439826fed211bf40a2d87fb50eb3
Arpan Shingala <202003013@daiict.ac.in>
2022-02-04 22:32:28 +0530

    CheckPoint12

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 function App() {
     return (
         <>
-            <Router>
-                <NavBar />
-                <Routes>
-                    <Route exact path="/" element={<Home />}/>
-                    <Route exact path="/about" element={<About />}/>
-                </Routes>
-            </Router>
+            <NoteState>
+                <Router>
+                    <NavBar />
+                    <div className="container">
+                        <Routes>
+                            <Route exact path="/" element={<Home />} />
+                            <Route exact path="/about" element={<About />} />
+                        </Routes>
+                    </div>
+                </Router>
+            </NoteState>
         </>
     );
 }
