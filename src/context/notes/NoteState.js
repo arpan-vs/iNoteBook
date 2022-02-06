ae54229e6ed44ab00a89079ec755d68edd996913
Arpan Shingala <202003013@daiict.ac.in>
2022-02-06 12:33:00 +0530

    CheckPoint15

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -77,9 +77,37 @@ const NoteState = (props) =>{
           "__v": 0
         }
       ]
+
       const [notes, setNotes] = useState(notesInitial)
+
+
+      // Add a Note
+      const addNote = (title, description, tag) => {
+        // TODO: API call
+        console.log("Hhello")
+        const note = {
+          "_id": "61fcac2ca7sc9e88sb2df033af",
+          "user": "61fc04e11b1478fcd9287ab2",
+          "title": title,
+          "description": description,
+          "tag": tag,
+          "date": "2022-02-04T04:31:40.546Z",
+          "__v": 0
+        };
+        setNotes(notes.concat(note));
+      }
+
+      // Delete a Note
+      const deleteNote = () => {
+
+      }
+      
+      // Edit a Note
+      const editNote = () => {
+
+      }
     return (
-        <NoteContext.Provider value={{notes,setNotes}}>
+        <NoteContext.Provider value={{notes,addNote, deleteNote, editNote}}>
             {props.children}
         </NoteContext.Provider>
     )
